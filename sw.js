2e6a71637141e9bd2ca09a1e8bc24774400a7930
khalednmg100-dotcom <khalednmg100@gmail.com>
2026-09-07 08:57:56 +0300

    Rebuild app: separate surah/juz tracking, daily wird, morning/evening adhkar, notifications, settings

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "wirdi-cache-v1";
+const CACHE_NAME = "wirdi-cache-v2";
 const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function(event) {
